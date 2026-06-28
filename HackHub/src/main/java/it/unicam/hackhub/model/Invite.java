f0aaaa94418f0e0ea9298da2dbe35ef98b381f73
Mariagrazia Commone <mariagrazia.commone@studenti.unicam.it>
2026-06-28 15:58:25 +0200

    prime classi

---
+++ HackHub/src/main/java/it/unicam/hackhub/model/Invite.java
@@ -0,0 +1,35 @@
+package it.unicam.hackhub.model;
+
+public class Invite {
+    private String id;
+    private InviteState state;
+    private PartecipationRequest partecipationRequest;
+
+    public enum InviteState {
+        PENDING,
+        ACCEPTED,
+        DECLINED
+    } 
+
+    public enum PartecipationRequest {
+        PENDING,
+        ACCEPTED,
+        DECLINED
+    }
+
+    public Invite(String id, InviteState state, PartecipationRequest partecipationRequest) {
+        this.id = id;
+        this.state = state;
+        this.partecipationRequest = partecipationRequest;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public InviteState getState() {
+        return state;
+    }
+    public PartecipationRequest getPartecipationRequest() {
+        return partecipationRequest;
+    }
+}
--- HackHub/src/main/java/it/unicam/hackhub/model/MembroStaff.java
+++ HackHub/src/main/java/it/unicam/hackhub/model/MembroStaff.java
@@ -2,5 +2,18 @@ package it.unicam.hackhub.model;
 
 public abstract class MembroStaff {
     private String ruolo;
+    private String userId;
 
+    public MembroStaff(String ruolo, String userId) {
+        this.ruolo = ruolo;
+        this.userId = userId;
+    }
+
+    public String getRuolo() {
+        return ruolo;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
 }
